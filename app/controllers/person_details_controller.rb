480467939eceaf2a2d3ce9de672f7ebc17fa81cb
samvaran kashyap <samvaran2best@gmail.com>
2015-02-17 01:45:37 -0600

    initial export

--- app/controllers/person_details_controller.rb
+++
@@ -1,6 +0,0 @@
-class PersonDetailsController < ApplicationController
-  def index
-    render json: PersonDetail.all
-  def show
-    render json: PersonDetail.find(params[:id])
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 ApplicationSkr::Application.routes.draw do
+  get 'welcome/index'
+
   scope :api do
     get "/persondetails(.:format)" => "persondetails#index"
     get "/persondetails/:id(.:format)" => "persondetails#show"
   end
+ root to: "welcome#index"
 end
 
 #Rails.application.routes.draw do
+#  get 'welcome/index'
+
 #  resources :person_details
 
   # The priority is based upon order of creation: first created -> highest priority.
