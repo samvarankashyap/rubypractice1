1a55603d79f0a04e73ee0d1c2b469c9c5a2e37bd
= <=samvaran2best@gmail.com>
2015-02-17 21:40:59 -0600

    added seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+PersonDetail.create(id: 1 , first_name: "samvaran kashyap",last_name:"rallabandi", dob: "1992-08-21 00:00:00", email: "samvaran.kashyap@gmail.com", location: "Texas Arlington",latitude: "32.72909", longitude: "-97.10627",about_me: "A unique name to begin with , a passionate learner to end with .", created_at: "2015-02-16", updated_at: "2015-02-16")
+PersonDetail.create(id: 2 , first_name: "Choti",last_name:"Tasmeen", dob: "1992-09-18 00:00:00", email: "tasmeen.choti@gmail.com", location: "Texas Arlington",latitude: "32.72909", longitude: "-97.10627",about_me: "Hai Hello its me.", created_at: "2015-02-16", updated_at: "2015-02-16")
+PersonDetail.create(id: 3 , first_name: "abc",last_name:"xyz", dob: "1992-08-21 00:00:00", email: "abc.xyz@gmail.com", location: "Sydney",latitude: "-33.8569", longitude: "151.2152",about_me: "ABCXYZ", created_at: "2015-02-16", updated_at: "2015-02-16")
